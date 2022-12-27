9cbc56133b5a912c1f359cdee6dd2bfa0fb80c22
surajpaul2412 <48125960+surajpaul2412@users.noreply.github.com>
2022-12-27 18:16:00 +0530

    Add files via upload

--- database/migrations/2022_10_02_173808_create_cities_table.php
+++ database/migrations/2022_10_02_173808_create_cities_table.php
@@ -18,6 +18,7 @@ class CreateCitiesTable extends Migration
             $table->string('name');
             $table->integer('country_id');
             $table->string('avatar')->default('uploads/city/default.jpg');
+            $table->integer('seal')->default(0);
             $table->timestamps();
         });
     }

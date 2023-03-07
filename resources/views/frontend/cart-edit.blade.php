8beb7d57528bfdfc234b16a30edf1f1fdd6c1f8a
surajpaul2412 <suraj.paul.69@gmail.com>
2023-03-08 03:15:32 +0530

    7

--- resources/views/frontend/cart-edit.blade.php
+++ resources/views/frontend/cart-edit.blade.php
@@ -440,4 +440,57 @@ $mightAlsoLike = Package::where('slug','!=',$tour->slug)->whereCityId($tour->cit
 		});
 	});
 </script>
+
+<script>
+	// Quantity buttons
+	function qtySum(){
+	    var arr = document.getElementsByClassName('qtyInput');
+	    var tot=0;
+	    for(var i=0;i<arr.length;i++){
+	        if(parseInt(arr[i].value))
+	            tot += parseInt(arr[i].value);
+	    }
+
+	    var cardQty = document.querySelector(".qtyTotal");
+	    cardQty.innerHTML = tot;
+	    if (tot > <?php echo $tour->capacity-1; ?>) {
+	    	$(".qtyInc").hide();
+	    } else {
+	    	$(".qtyInc").show();
+	    }
+	}
+	qtySum();
+
+	$(function() {
+
+	   $(".qtyButtons input").after('<div class="qtyInc"></div>');
+	   $(".qtyButtons input").before('<div class="qtyDec"></div>');
+	   $(".qtyDec, .qtyInc").on("click", function() {
+
+		  var $button = $(this);
+		  var oldValue = $button.parent().find("input").val();
+
+		  if ($button.hasClass('qtyInc')) {
+			 var newVal = parseFloat(oldValue) + 1;
+		  } else {
+			 // don't allow decrementing below zero
+			 if (oldValue > 0) {
+				var newVal = parseFloat(oldValue) - 1;
+			 } else {
+				newVal = 0;
+			 }
+		  }
+
+		  $button.parent().find("input").val(newVal);
+		  qtySum();
+		  $(".qtyTotal").addClass("rotate-x");
+
+	   });
+
+	   function removeAnimation() { $(".qtyTotal").removeClass("rotate-x"); }
+	   const counter = document.querySelector(".qtyTotal");
+	   counter.addEventListener("animationend", removeAnimation);
+
+	});
+</script>
 @endsection
